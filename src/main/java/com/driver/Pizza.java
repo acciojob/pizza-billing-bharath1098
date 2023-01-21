cb7b3edd4ab43cf46b07a90faf5b0d315d1ff0b7
Bharath kumar K <2021bharathkumar@gmail.com>
2023-01-21 13:42:39 +0530

    Solution

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,79 +5,71 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-   private int vegpizza=0;
-   private int nonpizza=0;
+    private boolean isExtracheese;
+    private boolean isExtraTopping;
+    private boolean isbill;
+    private boolean iscarrybag;
+    private int extracheese;
+    private int extratopping;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        this.price=0;
         this.bill="";
+        this.isExtracheese=false;
+        this.isExtraTopping=false;
+        this.isbill=false;
+        this.iscarrybag=false;
+
+
         if(this.isVeg==true){
-            vegpizza++;
             this.price+=300;
+            this.extratopping=70;
         }
         else{
-            nonpizza++;
             this.price+=400;
+            this.extratopping=120;
         }
+        this.extracheese=80;
+        this.bill+="Base Price Of The Pizza: "+this.price;
+        this.bill+="\n";
     }
     public int getPrice(){
         return this.price;
     }
-    private int extracheese=0;
+
     public void addExtraCheese(){
-        extracheese++;
-        this.price+=80;
+        if(isExtracheese==false) {
+            this.price +=extracheese;
+            isExtracheese=true;
+        }
     }
-        private int extratopveg=0;
-        private int extratopnon=0;
+
     public void addExtraToppings(){
-        if(this.isVeg==true) {
-            extratopveg++;
-            this.price += 70;
-        }
-        else{
-            extratopnon++;
-            this.price += 120;
-        }
+       if(isExtraTopping==false){
+           this.price+=extratopping;
+           isExtraTopping=true;
+       }
     }
-    private int paperbag=0;
     public void addTakeaway(){
-        paperbag++;
+        if(iscarrybag==false)
         this.price+=20;
+        iscarrybag=true;
     }
 
     public String getBill(){
-        if(this.isVeg==true && vegpizza>0){
-            this.bill+="Veg pizza base price: "+(300*vegpizza);
-            this.bill+="\n";
+        if(isbill==false){
+            if(isExtracheese) {
+                this.bill += "Extra Cheese Added: " + extracheese + "\n";
+            }
+            if(isExtraTopping){
+                this.bill+="Extra Toppings Added: " +extratopping+"\n";
+            }
+            if(iscarrybag){
+                this.bill+="Paperbag Added: " +20+"\n";
+            }
+            this.bill+="Total Price: "+this.price+"\n";
+            isbill=true;
+        }
 
-             }
-        else{
-            this.bill+="Non-veg pizza base price: "+(400 * nonpizza);
-            this.bill+="\n";
-//            System.out.println("Non-veg pizza base price: "+(400 * nonpizza));
-        }
-        if(extracheese>0) {
-            this.bill+="Extra Cheese Added: " + (80*extracheese);
-            this.bill+="\n";
-//            System.out.println("Extra Cheese Added: " + (80*extracheese));
-        }
-        if(this.isVeg==true && extratopveg>0) {
-            this.bill+="Extra Toppings Added: " + (70*extratopveg);
-            this.bill+="\n";
-//            System.out.println("Extra Toppings Added: " + (70*extratopveg));
-        }
-        else if(this.isVeg==false && extratopnon>0){
-            this.bill+="Extra Toppings Added: " + (120*extratopnon);
-            this.bill+="\n";
-//            System.out.println("Extra Toppings Added: " + (120*extratopnon));
-        }
-        if(paperbag>0) {
-            this.bill+="Paperbag Added: " + (20 * paperbag);
-            this.bill+="\n";
-//            System.out.println("Paperbag Added: " + (20 * paperbag));
-        }
-        this.bill+="Total Price: "+this.price;
         return this.bill;
     }
 }
